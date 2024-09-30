f7970df6c1f2e0982333b3788793fcdc5f5816ae
jBiasi99 <144751547+jBiasi99@users.noreply.github.com>
2024-09-30 09:00:22 -0300

    Create calculadora.py

---
+++ calculadora.py
@@ -0,0 +1,15 @@
+# arquivo: calculadora.py
+
+def soma(a, b):
+    return a + b
+
+def subtracao(a, b):
+    return a - b
+
+def multiplicacao(a, b):
+    return a * b
+
+def divisao(a, b):
+    if b == 0:
+        raise ValueError("Divisão por zero!")
+    return a / b
